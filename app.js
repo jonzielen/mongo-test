8e77d524398ba4ce221620e9f770f44502f3ca2a
Jon Zielenkievicz <jonzielen@gmail.com>
2016-01-01 16:27:33 -0500

    update mongo connection type

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var db = require('./app/db');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var feeds = require('./routes/feeds');
 
 var app = express();
+
+// Connect to Mongo on start
+db.connect('mongodb://localhost:27017/test', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/feeds', feeds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
